453c711944b60d51e50a79185bf1d3f410b1caf9
Tomas Perez <tomasitopg6@gmail.com>
2020-06-19 03:57:29 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,7 +35,7 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
   
-  shipping_manifest[]
+  shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
